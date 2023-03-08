352cb31942723b78663df80db67f7eed72abdf11
Marek Serafin <marek@snowheads.pl>
2023-03-08 18:31:55 +0100

    All

---
+++ Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by Marek Serafin on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
